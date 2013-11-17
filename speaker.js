3148c85245c11f312e08c695d1c09a754c9386c2
Pavel Smolka <pavel.smolka@gmail.com>
2013-11-17 19:18:36 +0000

    Custom audio container is allowed

--- speaker.js
+++ speaker.js
@@ -2,19 +2,26 @@
  * Created by pavel on 11/17/13.
  */
 
-Speaker = (function() {
+Speaker = (function($) {
 
-    this.DEFAULT_LANGUAGE = 'en';
+    this.audioContainer = 'body';
+    this.defaultLanguage = 'en';
 
     this.speak = function(speech, language) {
+
         if (!language) {
-            language = this.DEFAULT_LANGUAGE;
+            language = this.defaultLanguage;
         }
-        var url = 'http://translate.google.com/translate_tts?tl=' + language + '&q=' + speech;
-        $('body').append('<audio />');
-        $("audio").attr("src", url);
+
+        var soundUrl = 'http://translate.google.com/translate_tts?tl=' + language + '&q=' + speech;
+
+        $('<audio />').appendTo(this.audioContainer).attr("src", soundUrl);
+
         document.getElementsByTagName('audio')[0].play();
-    }
+
+        // the audio element should be reused or removed after
+
+    };
 
     return this;
 
